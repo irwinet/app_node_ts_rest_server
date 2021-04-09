42ac630191f84defd259d7856c7e40bb209c47af
Irwin <irwinestradatorres@gmail.com>
2021-04-09 16:40:21 -0500

    CRUD Usuarios And Install Sequelize

--- models/server.ts
+++ models/server.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 
 import userRoutes from '../routes/usuario';
 import cors from 'cors';
+import db from '../db/connection';
 
 class Server {
     
@@ -16,11 +17,20 @@ class Server {
         this.port = process.env.PORT || '8080';
 
         //Metodos Iniciales
+        this.dbConnection();
         this.middlewares();
         this.routes();        
     }
 
     // TODO: Conectar base de datos
+    async dbConnection(){
+        try {
+            await db.authenticate();
+            console.log('Database Online');
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
 
     middlewares(){
         // CORS
